29c2995ecb80367d54e671df1873f429096d3376
Yousuf Khan <ratulcse27@gmail.com>
2022-04-26 20:19:34 +0200

    Add computer vs computer battle

--- src/app/pages/computer-vs-computer/computer-vs-computer.component.ts
+++ src/app/pages/computer-vs-computer/computer-vs-computer.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { randomEnum } from 'src/app/helpers';
+import { GameStatus, Options, Result } from 'src/app/models/game';
+import { CalculateResultService } from 'src/app/services/result/calculate-result.service';
 
 @Component({
   selector: 'app-computer-vs-computer',
   templateUrl: './computer-vs-computer.component.html',
-  styleUrls: ['./computer-vs-computer.component.scss']
+  styleUrls: ['./computer-vs-computer.component.scss'],
 })
-export class ComputerVsComputerComponent implements OnInit {
+export class ComputerVsComputerComponent {
+  constructor(private readonly calculateResultService: CalculateResultService) {
+    this.board = this.initializeResult();
+  }
+  public board: Result;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public get isInitialState(): boolean {
+    return this.board.gameStatus === GameStatus.INITIAL_STATE;
   }
 
+  public get isCompleted(): boolean {
+    return this.board.gameStatus === GameStatus.COMPLETED;
+  }
+
+  public get gameOptions(): typeof Options {
+    return Options;
+  }
+
+  public get roundNumber(): number {
+    return this.board.roundNumber;
+  }
+
+  public get result(): Result {
+    return this.board;
+  }
+
+  public playGame(): void {
+    this.board = {
+      ...this.board,
+      player1: randomEnum(Options),
+      player2: randomEnum(Options),
+      gameStatus: GameStatus.COMPLETED,
+      roundNumber: this.board.roundNumber + 1,
+    };
+    this.board = this.calculateResultService.calculateResult(this.board);
+    console.log(this.board);
+  }
+
+  public playAgain(): void {
+    this.resetPlayersChoice();
+  }
+
+  public restart(): void {
+    this.board = this.initializeResult();
+  }
+
+  private initializeResult(): Result {
+    return {
+      player1: null,
+      player2: null,
+      result: 'Please press start battle button',
+      player1Score: 0,
+      player2Score: 0,
+      gameStatus: GameStatus.INITIAL_STATE,
+      roundNumber: 1,
+    };
+  }
+
+  private resetPlayersChoice(): void {
+    this.board = {
+      ...this.board,
+      player1: null,
+      player2: null,
+      result: 'Please press start battle button',
+      gameStatus: GameStatus.INITIAL_STATE,
+    };
+  }
 }
