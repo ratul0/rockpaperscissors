172ea271159a7ba10410df533029d2f907f5e56f
Yousuf Khan <ratulcse27@gmail.com>
2022-04-26 20:53:16 +0200

    Add not found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
+  constructor(private readonly router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public goToHomePage(): void {
+    this.router.navigateByUrl('/home');
   }
-
 }
