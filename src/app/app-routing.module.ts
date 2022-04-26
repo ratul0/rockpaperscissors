0d88823530b53d79861734d00911625ec719e905
Yousuf Khan <ratulcse27@gmail.com>
2022-04-26 20:19:34 +0200

    Add computer vs computer battle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComputerVsComputerComponent } from './pages/computer-vs-computer/computer-vs-computer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PlayerVsComputerComponent } from './pages/player-vs-computer/player-vs-computer.component';
@@ -7,6 +8,7 @@ import { PlayerVsComputerComponent } from './pages/player-vs-computer/player-vs-
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'player-vs-computer', component: PlayerVsComputerComponent },
+  { path: 'computer-vs-computer', component: ComputerVsComputerComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
